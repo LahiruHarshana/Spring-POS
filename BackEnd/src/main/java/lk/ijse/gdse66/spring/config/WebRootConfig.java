a4ae3f6b773cb2bd18f7d32546bae85d73c1f6d3
LahiruHarshana <lharshana2002@gmail.com>
2024-03-30 08:36:00 +0530

    added WebRootConfig

---
+++ BackEnd/src/main/java/lk/ijse/gdse66/spring/config/WebRootConfig.java
@@ -0,0 +1,24 @@
+package lk.ijse.gdse66.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.xml.transform.Transformer;
+
+/**
+ * @author : L.H.J
+ * @File: WebRootConfig
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-03-14, Thursday
+ **/
+
+@Configuration
+@ComponentScan(basePackages = {"lk.ijse.spring.service","lk.ijse.spring.repositories"} , basePackageClasses = Transformer.class)
+
+@EnableWebMvc
+@Import({JPAConfig.class})
+public class WebRootConfig {
+
+}
