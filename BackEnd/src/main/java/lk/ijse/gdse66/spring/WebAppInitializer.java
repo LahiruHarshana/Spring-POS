d904c74a56d6b3d0cecd893ee083f988f51d1154
LahiruHarshana <lharshana2002@gmail.com>
2024-03-30 08:32:48 +0530

    updated

---
+++ BackEnd/src/main/java/lk/ijse/gdse66/spring/WebAppInitializer.java
@@ -0,0 +1,10 @@
+package lk.ijse.gdse66.spring;
+
+/**
+ * @author : L.H.J
+ * @File: WebAppInitializer
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-03-30, Saturday
+ **/
+public class WebAppInitializer {
+}
