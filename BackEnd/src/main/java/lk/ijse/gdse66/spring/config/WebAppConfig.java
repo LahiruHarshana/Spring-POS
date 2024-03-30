e5af8f647e3480d5bba3cfdb6931e55d3f2152f9
LahiruHarshana <lharshana2002@gmail.com>
2024-03-30 08:35:38 +0530

    added WebAppConfig

---
+++ BackEnd/src/main/java/lk/ijse/gdse66/spring/config/WebAppConfig.java
@@ -0,0 +1,20 @@
+package lk.ijse.gdse66.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+/**
+ * @author : L.H.J
+ * @File: WebAppConfig
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-03-14, Thursday
+ **/
+
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"lk.ijse.spring.api","lk.ijse.spring.advice"})
+public class WebAppConfig {
+
+}
