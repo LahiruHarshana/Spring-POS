573c91090b13af9b40ca3c9acf5cea1d8c966832
LahiruHarshana <lharshana2002@gmail.com>
2024-03-30 10:54:56 +0530

    added crudService

--- BackEnd/src/main/java/lk/ijse/gdse66/spring/service/CustomerService.java
+++ BackEnd/src/main/java/lk/ijse/gdse66/spring/service/CustomerService.java
@@ -1,11 +1,14 @@
 package lk.ijse.gdse66.spring.service;
 
+import lk.ijse.gdse66.spring.dto.CustomerDTO;
+import lk.ijse.gdse66.spring.entity.Customer;
+
 /**
  * @author : L.H.J
  * @File: CustomerService
  * @mailto : lharshana2002@gmail.com
  * @created : 2024-03-30, Saturday
  **/
-public interface CustomerService {
+public interface CustomerService extends CrudService<CustomerDTO>{
 
 }
---
+++ BackEnd/src/main/java/lk/ijse/gdse66/spring/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,39 @@
+package lk.ijse.gdse66.spring.service.impl;
+
+import lk.ijse.gdse66.spring.dto.CustomerDTO;
+import lk.ijse.gdse66.spring.service.CustomerService;
+
+import java.util.List;
+
+/**
+ * @author : L.H.J
+ * @File: CustomerServiceImpl
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-03-30, Saturday
+ **/
+public class CustomerServiceImpl implements CustomerService {
+    @Override
+    public void save(CustomerDTO customerDTO) {
+
+    }
+
+    @Override
+    public void update(CustomerDTO customerDTO) {
+
+    }
+
+    @Override
+    public void delete(String id) {
+
+    }
+
+    @Override
+    public CustomerDTO search(String id) {
+        return null;
+    }
+
+    @Override
+    public List<CustomerDTO> getAll() {
+        return null;
+    }
+}
