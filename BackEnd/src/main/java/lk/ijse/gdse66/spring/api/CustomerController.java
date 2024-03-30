5a304b4b41a080e9e4a7fe959ef8fa8befa51a88
LahiruHarshana <lharshana2002@gmail.com>
2024-03-30 10:02:08 +0530

    added customer controller

--- BackEnd/src/main/java/lk/ijse/gdse66/spring/api/CustomerController.java
+++ BackEnd/src/main/java/lk/ijse/gdse66/spring/api/CustomerController.java
@@ -1,10 +1,24 @@
 package lk.ijse.gdse66.spring.api;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author : L.H.J
  * @File: CustomerController
  * @mailto : lharshana2002@gmail.com
  * @created : 2024-03-30, Saturday
  **/
+
+@RestController
+@RequestMapping("/api/v1/customer")
+@CrossOrigin(origins = "*")
 public class CustomerController {
+
+    @PostMapping
+    public void saveCustomer(){
+        System.out.println("Customer Save");
+    }
 }
