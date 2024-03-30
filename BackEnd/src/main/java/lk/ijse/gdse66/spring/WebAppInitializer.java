50b5caa64000772f74572ff141435f9a0b34617c
LahiruHarshana <lharshana2002@gmail.com>
2024-03-30 08:35:15 +0530

    added WebAppInitializer

--- BackEnd/src/main/java/lk/ijse/gdse66/spring/WebAppInitializer.java
+++ BackEnd/src/main/java/lk/ijse/gdse66/spring/WebAppInitializer.java
@@ -1,11 +1,28 @@
 package lk.ijse.gdse66.spring;
 
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
 /**
  * @author : L.H.J
  * @File: WebAppInitializer
  * @mailto : lharshana2002@gmail.com
  * @created : 2024-03-30, Saturday
  **/
-public class WebAppInitializer {
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
 
 }
---
+++ BackEnd/src/main/java/lk/ijse/gdse66/spring/config/JPAConfig.java
@@ -0,0 +1,70 @@
+package lk.ijse.gdse66.spring.config;
+
+import jakarta.persistence.EntityManagerFactory;
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+/**
+ * @author : L.H.J
+ * @File: JpaConfig
+ * @mailto : lharshana2002@gmail.com
+ * @created : 2024-03-25, Monday
+ **/
+
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = "lk.ijse.spring.repositories")
+@Configuration
+public class JPAConfig {
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dmds = new DriverManagerDataSource();
+        dmds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dmds.setUsername("root");
+        dmds.setUrl("jdbc:mysql://localhost:3306/gdse66_spring_web?createDatabaseIfNotExist=true");
+        dmds.setPassword("12345678");
+        return dmds;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(true);
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setPackagesToScan("lk.ijse.spring.entity");
+        factory.setDataSource(dataSource());
+        return factory;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+        JpaTransactionManager txManager = new JpaTransactionManager();
+        txManager.setEntityManagerFactory(entityManagerFactory);
+        return txManager;
+    }
+
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+    }
+}
+
+
+
+
+
+
+
+
+
