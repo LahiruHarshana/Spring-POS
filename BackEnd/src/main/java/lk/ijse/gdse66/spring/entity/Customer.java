4ff8d940d10c41ade5898fde1ba60292e5325ab7
LahiruHarshana <lharshana2002@gmail.com>
2024-03-30 09:27:21 +0530

    added Customer

--- BackEnd/src/main/java/lk/ijse/gdse66/spring/entity/Customer.java
+++ BackEnd/src/main/java/lk/ijse/gdse66/spring/entity/Customer.java
@@ -1,10 +1,36 @@
 package lk.ijse.gdse66.spring.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author : L.H.J
  * @File: Customer
  * @mailto : lharshana2002@gmail.com
  * @created : 2024-03-30, Saturday
  **/
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "customer")
 public class Customer {
+    @Id
+    @Column(name = "cus_id")
+    private String cus_id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "salary")
+    private double salary;
 }
